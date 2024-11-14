3ff36f7f76de8aff2501f51c34ff2899e90e4d5e
garda1000 <D1041221042@student.untan.ac.id>
2024-11-14 15:21:34 +0700

    a

--- scriptFlow.js
+++ scriptFlow.js
@@ -1,7 +1,16 @@
 const keterangan = sessionStorage.getItem("keterangan");
-document.getElementById("output").innerHTML = keterangan;
+if (keterangan) {
+    document.getElementById("output").innerHTML = keterangan;
+} 
 
-if (keterangan.length > 3) {
+const tanggal = sessionStorage.getItem("tanggal");
+if (tanggal) {
+    document.getElementById("date").innerHTML = tanggal;
+    document.getElementById("tgl").innerHTML = tanggal;
+}
+
+
+if (keterangan && keterangan.length > 2) {
     document.getElementById("terkirimStep").classList.remove("hidden");
     document.getElementById("selesaiStep").classList.remove("hidden");
 }

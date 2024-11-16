3c8413b3f13b6a0e3423af95c8746a24911cc2bc
garda1000 <D1041221042@student.untan.ac.id>
2024-11-16 14:04:15 +0700

    time stamp buat tracking

--- scriptFlow.js
+++ scriptFlow.js
@@ -1,15 +1,25 @@
+// Ambil data dari sessionStorage
 const keterangan = sessionStorage.getItem("keterangan");
-if (keterangan) {
-    document.getElementById("output").innerHTML = keterangan;
-} 
-
+const lastUpdate = sessionStorage.getItem("LastUpdate");
 const tanggal = sessionStorage.getItem("tanggal");
-if (tanggal) {
-    document.getElementById("date").innerHTML = tanggal;
-    document.getElementById("tgl").innerHTML = tanggal;
+
+// Masukkan keterangan ke elemen "output"
+if (keterangan) {
+    document.getElementById("output").innerText = keterangan;
 }
 
+if (tanggal) {
+    document.getElementById("datedetik").innerText = tanggal;
+    document.getElementById("tgldetik").innerText = tanggal;
+}
 
+// Masukkan tanggal ke elemen yang sesuai
+if (lastUpdate) {
+    document.getElementById("date").innerText = lastUpdate;
+    document.getElementById("tgl").innerText = lastUpdate;
+}
+
+// Tampilkan langkah tambahan jika keterangan tersedia
 if (keterangan && keterangan.length > 2) {
     document.getElementById("terkirimStep").classList.remove("hidden");
     document.getElementById("selesaiStep").classList.remove("hidden");

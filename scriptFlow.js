82f5cec86d96e76a5d12c5e3a34e2f4f8da74183
garda1000 <D1041221042@student.untan.ac.id>
2024-11-18 14:58:44 +0700

    GANTI TEXT

--- scriptFlow.js
+++ scriptFlow.js
@@ -3,7 +3,6 @@ const keterangan = sessionStorage.getItem("keterangan");
 const lastUpdate = sessionStorage.getItem("LastUpdate");
 const tanggal = sessionStorage.getItem("tanggal");
 
-// Masukkan keterangan ke elemen "output"
 if (keterangan) {
     document.getElementById("output").innerText = keterangan;
 }
@@ -13,14 +12,12 @@ if (tanggal) {
     document.getElementById("tgldetik").innerText = tanggal;
 }
 
-// Masukkan tanggal ke elemen yang sesuai
 if (lastUpdate) {
     document.getElementById("date").innerText = lastUpdate;
     document.getElementById("tgl").innerText = lastUpdate;
 }
 
-// Tampilkan langkah tambahan jika keterangan tersedia
-if (keterangan && keterangan.length > 2) {
+if (keterangan && keterangan.length > 1) {
     document.getElementById("terkirimStep").classList.remove("hidden");
     document.getElementById("selesaiStep").classList.remove("hidden");
 }

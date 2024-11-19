6bbe2ab1abdee023c1dcaf0a466e1a0636d88ad7
garda1000 <D1041221042@student.untan.ac.id>
2024-11-19 10:10:32 +0700

    fitur password

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 function showLoader(event, url) {
-    event.preventDefault(); 
+    if (event) event.preventDefault();
     document.getElementById('spinner').style.display = 'block';
 
     setTimeout(() => {
         window.location.href = url;
     }, 2000);
+}
+
+function showPasswordModal() {
+    document.getElementById('passwordModal').style.display = 'flex';
+    document.getElementById('errorMsg').textContent = ""; // Reset pesan error
+}
+
+function closePasswordModal() {
+    document.getElementById('passwordModal').style.display = 'none';
+}
+
+function validatePassword() {
+    const password = document.getElementById('passwordInput').value;
+    const correctPassword = "disperindag123";
+
+    if (password === correctPassword) {
+        closePasswordModal();
+        showLoader(null, "CRUD-Data.html");
+    } else {
+        document.getElementById('errorMsg').textContent = "❌ Password salah. Coba lagi!";
+    }
 }
